8a51118e6d8fb16bf997229c9759524f2306b18a
Lucas Luize <lucasluize.tech@gmail.com>
2021-12-20 20:09:47 -0500

    Part1 and Par2 Done. + time.txt"

---
+++ unroll.js
@@ -0,0 +1,73 @@
+function unroll(squareArray) {
+  let result = [];
+  let rows = squareArray.length;
+  let currentRow = 0;
+  let currentIdx = 0;
+  let iterations = 0;
+  let running = true;
+
+  const check = (array) => {
+    const result = array.length === squareArray.length ** 2;
+    return result ? (running = false) : running;
+  };
+
+  const leftToRight = (iteration) => {
+    if (!running) return;
+    currentRow = iteration;
+    while (currentIdx < rows) {
+      result.push(squareArray[currentRow][currentIdx + iteration]);
+      currentIdx++;
+    }
+    rows--;
+    check(result);
+  };
+
+  const TopToBottom = (iteration) => {
+    if (!running) return;
+    currentRow++;
+    currentIdx = squareArray.length - 1 - iteration;
+    if (currentRow > rows) {
+      result.push(squareArray[currentRow][currentIdx]);
+    }
+    while (currentRow <= rows) {
+      result.push(squareArray[currentRow][currentIdx]);
+      currentRow++;
+    }
+    check(result);
+  };
+
+  const rightToLeft = (iteration) => {
+    if (!running) return;
+    currentRow = currentIdx;
+    currentIdx--;
+    while (currentIdx >= 0 && currentIdx >= iteration) {
+      result.push(squareArray[currentRow][currentIdx]);
+      currentIdx--;
+    }
+    rows--;
+    check(result);
+  };
+
+  const bottomToTop = () => {
+    if (!running) return;
+    currentIdx = 0;
+    currentRow--;
+    while (currentRow > 0) {
+      result.push(squareArray[currentRow][currentIdx]);
+      currentRow--;
+    }
+    check(result);
+  };
+
+  while (running) {
+    leftToRight(iterations);
+    TopToBottom(iterations);
+    rightToLeft(iterations);
+    bottomToTop();
+    iterations++;
+  }
+
+  return result;
+}
+
+module.exports = unroll;
